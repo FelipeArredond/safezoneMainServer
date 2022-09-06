137d711bede9cd9ccc8ae97cb0ca6b924af4c22e
FelipeArredond <pipechiqui77@gmail.com>
2022-09-06 09:31:53 -0500

    Second Release

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const routerApi = require('./routes');
+const cors = require('cors')
 const app = express();
 const port = 3500;
 
 app.use(express.json())
+
+app.use(cors());
 
 routerApi(app);
 
--- routes/barriosRouter.js
+++ routes/barriosRouter.js
@@ -3,14 +3,22 @@ const barrios = require('../barrios')
 
 const router = express.Router();
 
-barrios.features.map((feature) => {
-  feature.properties.SECURITYLEVEL = 0;
-})
+// barrios.features.map((feature) => {
+//   feature.properties.SECURITYLEVEL = 0;
+// })
+
+const barriosData = [];
+
+const barriosFiltering = () => {
+  barrios.features.map((feature) => {
+    barriosData.push(feature)
+  })
+}
+
+barriosFiltering();
 
 router.get(('/'),(req, res) => {
-  res.json(barrios.features.map((feature) => {
-    return feature.properties
-  }));
+  return res.json(barrios.features)
 })
 
 module.exports = router;
